5d85c81071751f46211213b9ed2ffe337309975f
Ryan Currie <ryan.currie@gmail.com>
2017-10-10 16:23:27 -0700

    Completed GET route to retreive representatives by ZIP from Google API #1.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
-const { MongoClient } = require('mongdb')
+const path = require('path')
+const { MongoClient } = require('mongodb')
+const superagent = require('superagent')
+const jsonParser = require('body-parser').json()
+
+const googleCivicKey = 'AIzaSyBql7aJLrYqP0dOY1nfo7kFoGjKsnQ4TVY'
 const app = express()
 
 const publicPath = path.join(__dirname, 'public')
 const staticMiddleware = express.static(publicPath)
 
 app.use(staticMiddleware)
+
+app.get('/get-reps/:zip', ({params: { zip }}, res) => {
+  superagent.get('https://www.googleapis.com/civicinfo/v2/representatives')
+  .query({ key: googleCivicKey, address: zip, roles: ['legislatorupperbody', 'legislatorlowerbody']})
+  .end((err, result) => {
+    if (err) {return console.log(err)}
+    res.json(result.body)
+  })
+})
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!')
