b4e417eb565def7d7aae0cf0ba1828d974e2407d
Ryan Currie <ryan.currie@gmail.com>
2017-10-20 13:14:43 -0700

    Changed port listening to work with heroku.

--- index.js
+++ index.js
@@ -255,7 +255,7 @@ MongoClient.connect(dbUrl, (err, db) => {
       })
   })
 
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000!')
   })
 })
